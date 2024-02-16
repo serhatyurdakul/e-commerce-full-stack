64b6f3a7db356c61c525bafffe6f227589db73f4
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2024-02-16 16:54:33 +0300

    Home Page Components Added
    Homepage Components Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,11 @@
 import "./App.css";
-import Footer from "./components/Layout/Footer/Footer";
-import Header from "./components/Layout/Header/Header";
-import Policy from "./components/Layout/Policy/Policy";
-import Sliders from "./components/Slider/Sliders";
-
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Sliders/>
-      <Policy/>
-      <Footer />
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 }
 
